936c6d2334e649f563c1451f2ae0944ba3247715
jesse webb <webbj19@ferris.edu>
2021-01-27 23:37:11 -0500

    made item script
    Added item script

---
+++ Assets/Scripts/Webb/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    [CreateAssetMenu(fileName = "New FishDataObject", menuName = "Fish Data Object")]
+    public class Item : ScriptableObject[CreateAssetMenu(fileName = "New Item", menuName = "Item Data Object")]
+public class Item : ScriptableObject { 
+       // Start is called before the first frame update
+       void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
