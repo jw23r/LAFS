a264cff0127987981d4b4dc7769ac4060fba8770
jesse webb <webbj19@ferris.edu>
2021-01-31 09:20:26 -0500

    Added other Scenes and menues

---
+++ Assets/Scripts/Webb/SceneMangment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMangment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void MainMenue()
+    {
+         SceneManager.LoadScene("MainMenue");
+
+    }
+    public void HighScore()
+    {
+        SceneManager.LoadScene("EndOfGame");
+
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("InStrunctions");
+
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("GamePlay");
+
+    }
+}
